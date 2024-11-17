68a28da1c41cd5b38348bed16f9a99894ed91f7b
Lion Kortlepel <development@kortlepel.com>
2024-11-17 20:31:58 +0100

    add AST building parser

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,7 @@ pub struct SourceToken {
     pub location: (usize, usize),
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Token {
     Add,
     Subtract,
@@ -173,6 +173,223 @@ fn consume<I: Iterator<Item = char>>(
 
 pub fn tokenize(code: &str) -> Result<Vec<SourceToken>, TokenizerError> {
     consume(code.chars().into_iter().peekable())
+}
+
+#[derive(Debug, PartialEq)]
+pub enum SyntaxNode {
+    Add,
+    Subtract,
+    MoveRight,
+    MoveLeft,
+    StackPush,
+    StackPop,
+    Function(String, Vec<AstNode>),
+    FuncCall(String),
+    Asm(String),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct AstNode {
+    node: SyntaxNode,
+    location: (usize, usize),
+}
+
+#[derive(Debug, PartialEq, Error)]
+pub enum InternalSyntaxError {
+    #[error("unexpected end of token stream")]
+    UnexpectedEof,
+    #[error("unexpected token: {0:?}")]
+    UnexpectedToken(Token),
+    #[error("unexpected token: {0:?}, instead expected {1:?}")]
+    UnexpectedTokenExpected(Token, Token),
+    #[error("expected token: {0:?}")]
+    Expected(Token),
+}
+
+#[derive(Debug, PartialEq, Error)]
+pub struct SyntaxError {
+    location: (usize, usize),
+    error: InternalSyntaxError,
+}
+
+pub fn build_syntax_tree(tokens: Vec<SourceToken>) -> Result<Vec<AstNode>, SyntaxError> {
+    build_ast(&mut tokens.into_iter().peekable(), None)
+}
+
+fn build_ast<I: Iterator<Item = SourceToken>>(
+    tokens: &mut Peekable<I>,
+    stop_at: Option<Token>,
+) -> Result<Vec<AstNode>, SyntaxError> {
+    let mut result = Vec::new();
+    let mut stop_found = if stop_at.is_some() { false } else { true };
+
+    while let Some(token) = tokens.next() {
+        match token {
+            SourceToken { token, .. } if Some(token.clone()) == stop_at => {
+                stop_found = true;
+                break;
+            }
+            SourceToken {
+                token: Token::Add,
+                location,
+            } => result.push(AstNode {
+                node: SyntaxNode::Add,
+                location,
+            }),
+            SourceToken {
+                token: Token::Subtract,
+                location,
+            } => result.push(AstNode {
+                node: SyntaxNode::Subtract,
+                location,
+            }),
+            SourceToken {
+                token: Token::MoveRight,
+                location,
+            } => result.push(AstNode {
+                node: SyntaxNode::MoveRight,
+                location,
+            }),
+            SourceToken {
+                token: Token::MoveLeft,
+                location,
+            } => result.push(AstNode {
+                node: SyntaxNode::MoveLeft,
+                location,
+            }),
+            SourceToken {
+                token: Token::StackPush,
+                location,
+            } => result.push(AstNode {
+                node: SyntaxNode::StackPush,
+                location,
+            }),
+            SourceToken {
+                token: Token::StackPop,
+                location,
+            } => result.push(AstNode {
+                node: SyntaxNode::StackPop,
+                location,
+            }),
+            SourceToken {
+                token: Token::FuncDecl,
+                location,
+            } => {
+                let (name, body) = consume_function(tokens, location)?;
+                result.push(AstNode {
+                    node: SyntaxNode::Function(name, body),
+                    location,
+                });
+            }
+            SourceToken {
+                token: Token::FuncCall,
+                location,
+            } => {
+                let name = consume_match_string(tokens, location)?;
+                let _ = consume_match_token(tokens, Token::Jawns, location)?;
+                result.push(AstNode {
+                    node: SyntaxNode::FuncCall(name),
+                    location,
+                });
+            }
+            SourceToken {
+                token: Token::AsmStart,
+                location,
+            } => {
+                let asm = consume_match_string(tokens, location)?;
+                let _ = consume_match_token(tokens, Token::Jawns, location)?;
+                result.push(AstNode {
+                    node: SyntaxNode::Asm(asm),
+                    location,
+                });
+            }
+            SourceToken { token, location } => {
+                return Err(SyntaxError {
+                    location,
+                    error: InternalSyntaxError::UnexpectedToken(token),
+                });
+            }
+        }
+    }
+
+    if !stop_found {
+        return Err(SyntaxError {
+            location: (0, 0),
+            error: InternalSyntaxError::Expected(stop_at.unwrap()),
+        });
+    }
+
+    Ok(result)
+}
+
+fn consume_function<I: Iterator<Item = SourceToken>>(
+    tokens: &mut Peekable<I>,
+    location: (usize, usize),
+) -> Result<(String, Vec<AstNode>), SyntaxError> {
+    let name = consume_match_string(tokens, location)?;
+    let _ = consume_match_token(tokens, Token::ScopeStart, location)?;
+    let mut body = Vec::new();
+    while let Some(token) = tokens.peek() {
+        match token {
+            SourceToken {
+                token: Token::ScopeEnd,
+                ..
+            } => {
+                let _ = tokens.next();
+                break;
+            }
+            // this will also fail if the scope end isnt found,
+            // so the above check only needs to exist for the empty body case
+            _ => body.append(&mut build_ast(tokens, Some(Token::ScopeEnd))?),
+        }
+    }
+    Ok((name, body))
+}
+
+fn consume_match_string<I: Iterator<Item = SourceToken>>(
+    tokens: &mut Peekable<I>,
+    location: (usize, usize),
+) -> Result<String, SyntaxError> {
+    if let Some(token) = tokens.next() {
+        if let Token::String(string) = token.token {
+            Ok(string)
+        } else {
+            Err(SyntaxError {
+                location,
+                error: InternalSyntaxError::UnexpectedTokenExpected(
+                    token.token,
+                    Token::String(String::new()),
+                ),
+            })
+        }
+    } else {
+        Err(SyntaxError {
+            location,
+            error: InternalSyntaxError::Expected(Token::String(String::new())),
+        })
+    }
+}
+
+fn consume_match_token<I: Iterator<Item = SourceToken>>(
+    tokens: &mut Peekable<I>,
+    token_to_match: Token,
+    location: (usize, usize),
+) -> Result<SourceToken, SyntaxError> {
+    if let Some(token) = tokens.next() {
+        if token.token == token_to_match {
+            Ok(token)
+        } else {
+            Err(SyntaxError {
+                location,
+                error: InternalSyntaxError::UnexpectedTokenExpected(token.token, token_to_match),
+            })
+        }
+    } else {
+        Err(SyntaxError {
+            location,
+            error: InternalSyntaxError::Expected(token_to_match),
+        })
+    }
 }
 
 #[cfg(test)]
@@ -538,5 +755,55 @@ mod tests {
                 location: (0, 0) // the location of error is before the token which failed to parse
             })
         )
+    }
+
+    #[test]
+    fn ast_simple_function() {
+        let input = vec![
+            SourceToken {
+                token: Token::FuncDecl,
+                location: (0, 0),
+            },
+            SourceToken {
+                token: Token::String(String::from("test")),
+                location: (0, 1),
+            },
+            SourceToken {
+                token: Token::ScopeStart,
+                location: (0, 5),
+            },
+            SourceToken {
+                token: Token::StackPop,
+                location: (0, 6),
+            },
+            SourceToken {
+                token: Token::StackPush,
+                location: (0, 7),
+            },
+            SourceToken {
+                token: Token::ScopeEnd,
+                location: (0, 8),
+            },
+        ];
+        let result = build_syntax_tree(input).unwrap();
+        assert_eq!(
+            result,
+            vec![AstNode {
+                node: SyntaxNode::Function(
+                    String::from("test"),
+                    vec![
+                        AstNode {
+                            node: SyntaxNode::StackPop,
+                            location: (0, 6),
+                        },
+                        AstNode {
+                            node: SyntaxNode::StackPush,
+                            location: (0, 7),
+                        },
+                    ]
+                ),
+                location: (0, 0),
+            }]
+        );
     }
 }
